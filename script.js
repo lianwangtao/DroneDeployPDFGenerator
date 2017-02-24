396cba09d98d256ea230e192f0af9f1ef2e9a29b
lianwangtao <wlian3@wisc.edu>
2017-02-24 14:32:50 -0600

    Add jsPDF

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-var zoom = 17;
-var layer = 'ortho';
-var id = null;
+//
+// function fetchTileDataFromPlan(api, plan){
+//   console.log(`Plan: `, plan);
+//   console.log(`API: `, api);
+//   id = plan.id;
+//   console.log(`ID: `, id);
+//   return api.Tiles.get({planId: plan.id.value, layerName: layer.value, zoom: parseInt(zoom.value)});
+// }
+//
+// function getTilesFromResponse(tileResponse){
+//   console.log(`Tile: `, tileResponse.tiles);
+//   return tileResponse.tiles;
+// }
+//
+// function updateTile(){
+//   new DroneDeploy({version: 1}).then(function(dronedeployApi){
+//      return dronedeployApi.Plans.getCurrentlyViewed();
+//   })
+//   .then(function(plan){
+//     console.log(`API in then: `, dronedeployApi)
+//     return fetchTileDataFromPlan(dronedeployApi, plan);
+//   })
+//   .then(getTilesFromResponse)
+//   .then(printOutput);
+// }
 
-function printOutput(tiles) {
-  console.log(`In output`);
-}
-
-function fetchTileDataFromPlan(plan){
-  console.log(`Plan: `, plan);
-  console.log(`API: `, dronedeployApi);
-  id = plan.id;
-  console.log(`ID: `, id);
-  return dronedeployApi.Tiles.get({planId: plan.id.value, layerName: layer.value, zoom: parseInt(zoom.value)});
-}
-
-function getTilesFromResponse(tileResponse){
-  console.log(`Tile: `, tileResponse.tiles);
-  return tileResponse.tiles;
-}
-
-function updateTile(){
-  new DroneDeploy({version: 1}).then(function(dronedeployApi){
-     return dronedeployApi.Plans.getCurrentlyViewed();
+function getPlan() {
+  return new DroneDeploy({version: 1}).then(function(dronedeployApi){
+    return dronedeployApi.Plans.getCurrentlyViewed()
   })
-  .then(function(plan, dronedeployApi){
-    return fetchTileDataFromPlan(plan);
-  })
-  .then(getTilesFromResponse)
-  .then(printOutput);
+}
+
+function downloadPDF() {
+  var doc = new jsPDF();
+  doc.text(20, 20, 'Hello world!');
+  doc.save('CurrentMap.pdf');
 }
