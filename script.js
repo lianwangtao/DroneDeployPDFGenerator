6c28170204edc132818e5cd6ba4d0243803c6d9b
lianwangtao <wlian3@wisc.edu>
2017-02-24 12:12:34 -0600

    Add API calls

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-var api = new DroneDeploy({version: 1}).then(function(api){
-  console.log('DroneDeploy Api: ', api);
-});
+var zoom = 17;
+var layer = 'ortho';
+var id = null;
+
+function printOutput(tiles) {
+  console.log(`In output`);
+}
+
+function fetchTileDataFromPlan(plan){
+  console.log(`Plan: `, plan);
+  console.log(`API: `, dronedeployApi);
+  id = plan.id;
+  console.log(`ID: `, id);
+  return dronedeployApi.Tiles.get({planId: plan.id.value, layerName: layer.value, zoom: parseInt(zoom.value)});
+}
+
+function getTilesFromResponse(tileResponse){
+  console.log(`Tile: `, tileResponse.tiles);
+  return tileResponse.tiles;
+}
+
+function updateTile(){
+  new DroneDeploy({version: 1}).then(function(dronedeployApi){
+     return dronedeployApi.Plans.getCurrentlyViewed();
+  })
+  .then(function(plan, dronedeployApi){
+    return fetchTileDataFromPlan(plan);
+  })
+  .then(getTilesFromResponse)
+  .then(printOutput);
+}
