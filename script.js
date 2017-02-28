65a48ffc60d291834fc012106135cc2c29c9f14a
lianwangtao <wlian3@wisc.edu>
2017-02-27 22:59:38 -0600

    409 Conflict Issue

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-//
-// function fetchTileDataFromPlan(api, plan){
-//   console.log(`Plan: `, plan);
-//   console.log(`API: `, api);
-//   id = plan.id;
-//   console.log(`ID: `, id);
-//   return api.Tiles.get({planId: plan.id.value, layerName: layer.value, zoom: parseInt(zoom.value)});
-// }
-//
-// function getTilesFromResponse(tileResponse){
-//   console.log(`Tile: `, tileResponse.tiles);
-//   return tileResponse.tiles;
-// }
-//
-// function updateTile(){
-//   new DroneDeploy({version: 1}).then(function(dronedeployApi){
-//      return dronedeployApi.Plans.getCurrentlyViewed();
-//   })
-//   .then(function(plan){
-//     console.log(`API in then: `, dronedeployApi)
-//     return fetchTileDataFromPlan(dronedeployApi, plan);
-//   })
-//   .then(getTilesFromResponse)
-//   .then(printOutput);
-// }
+const tileLinks = []
+const layer = 'ortho'
+const zoom = 17
 
-function getPlan() {
-  return new DroneDeploy({version: 1}).then(function(dronedeployApi){
+function downloadPDF() {
+  const doc = new jsPDF()
+  updateTile()
+  console.log(`titles #item: `, tileLinks.length)
+  for (const url in tileLinks) {
+    doc.addImage(url, 'JPEG', 15, 40, 180, 180)
+  }
+  doc.save('CurrentMap.pdf')
+}
+
+let dronedeployApiZ = null
+
+function updateTile(){
+  new DroneDeploy({version: 1}).then(function(dronedeployApi){
+    dronedeployApiZ = dronedeployApi
     return dronedeployApi.Plans.getCurrentlyViewed()
+  })
+  .then(function(plan){
+    return fetchTileDataFromPlan(dronedeployApiZ, plan)
+  })
+  .then(getTilesFromResponse)
+  .then(getLinks)
+}
+
+function fetchTileDataFromPlan(api, plan){
+  console.log(`PlanId: `, plan.id)
+  console.log(`Layer: `, layer)
+  console.log(`zoon: `, zoom)
+  console.log(`API: `, api)
+
+  return api.Tiles.get({
+    planId: plan.id,
+    layerName: layer,
+    zoom: parseInt(zoom)
   })
 }
 
-function downloadPDF() {
-  var doc = new jsPDF();
-  doc.text(20, 20, 'Hello world!');
-  doc.save('CurrentMap.pdf');
+function getTilesFromResponse(tileResponse){
+  console.log(`Get tiles from response`)
+  return tileResponse.tiles
+}
+
+function getLinks(linkURL) {
+  console.log(`linkURL: `, linkURL)
 }
